8c1ff51ddee40a27f16685b9953077c5b4c37938
Utkarsh Shukla <alancarter83@yahoo.com>
2020-06-13 17:24:20 +0530

    List of Todos added

--- To_Do_App/to_do_app.py
+++ To_Do_App/to_do_app.py
@@ -14,11 +14,18 @@ migrate = Migrate(app, db)
 class Todo(db.Model):
     __tablename__='todos'
     id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String, nullable=False)
+    description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
+
+class Todolist(db.Model):
+    __tablename__='todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', lazy=True)
 
 # db.create_all()
 
@@ -47,7 +54,9 @@ def set_completed_todos(todo_id):
     try:
         completed = request.get_json()['completed']
         todo = Todo.query.get(todo_id)
+        listid = Todolist.query.get(list_id)
         todo.completed = completed
+        todo.list_id = listid
         db.session.commit()
     except:
     
@@ -69,6 +78,15 @@ def delete_todo(todo_id):
     db.session.close()
   return redirect(url_for('index'))
 
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    return render_template('index.html',
+    lists = Todolist.query.all(),
+    active_list = Todolist.query.get(list_id),
+    todos =Todo.query.filter_by(list_id=list_id).order_by('id').all())
+
 @app.route('/')
 def index():
-    return render_template('index.html', data =Todo.query.order_by('id').all())
+    return redirect (url_for('get_list_todos', 
+    list_id=1))
+
